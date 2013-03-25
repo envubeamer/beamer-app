bed83f8d0f260584ca396cecd814e5074faf0947
Harri Eronen <harri.eronen@digia.com>
2013-03-25 16:10:18 +0200

    Dirty global handling

--- app/initialize.js
+++ app/initialize.js
@@ -5,9 +5,24 @@ $(function() {
 	    dataType: "json",
 	    contentType: "application/json"
 	});
+
 	$.ajaxPrefilter(function(options, originalOptions, xhr) {
 		application.setXhrHeaders(xhr)
 	});
+
+	
+	$(document).ajaxError(function (exception, xhr, options) {
+        if (xhr.status === 0) {
+            alert('No network connectivity.');
+        } else if (exception === 'timeout') {
+            alert('Request timed out.');
+        } else if (exception === 'abort') {
+            alert('Ajax request aborted.');
+        } else {
+        	var message = 'HTTP status code:' + xhr.status + '\n Error info:' + xhr.responseText;
+            alert(message.substr(0, 300));
+        }
+    });
 
     application.initialize()
     Backbone.history.start()
